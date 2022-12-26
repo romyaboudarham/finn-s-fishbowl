6d06c85dab53d75ca832d6901af9fa7a1b28b4cc
Romy Aboudarham <romy@romyaborhamsmbp.attlocal.net>
2022-12-25 20:18:28 -0800

    woooo got it all workinggit add .git add .

--- chapter-3/script.js
+++ chapter-3/script.js
@@ -1,15 +1,17 @@
+let dot = document.getElementById('dot');
+let countdown = document.getElementById('countdown');
+let cameraRig = document.getElementById('camera-rig');
+let camera = document.getElementById('camera');
+
 AFRAME.registerComponent("event-listener", {
     init: function() {
         this.el.addEventListener("click", function(e) {
-            var dot = document.getElementById('dot');
-            var countdown = document.getElementById('countdown')
             countdown.dispatchEvent(new CustomEvent('countdown-begin'))
             countdown.setAttribute('visible', 'true');
-            dot.parentNode.removeChild(dot);
+            disableDot();
         });
     }
   });
-
 
 AFRAME.registerComponent("countdown-animation-manager", {
     init: function() {
@@ -25,25 +27,62 @@ AFRAME.registerComponent("countdown-animation-manager", {
         this.el.emit("third")
       })
       this.el.addEventListener("animationcomplete__third", e => {
-        // start the second animation
-        // document.location.href = 'panic.html';
-        var entity = document.querySelector('[sound]');
-        document.getElementById('scene1').setAttribute('visible', 'false');
-        document.getElementById('scene2').setAttribute('visible', 'true');
-        entity.components.sound.playSound();
+        startScene2();
       })
     }
   });
 
-  AFRAME.registerComponent('restart-chapter', {
-    schema: {type: 'selector'},
-  
-    init: function () {
-      var soundEl = this.el;
-  
-      soundEl.addEventListener('sound-ended', function () {
-        document.location.href = 'index.html';
-      });
-    }
-  });
-  
+AFRAME.registerComponent('restart-chapter', {
+  schema: {type: 'selector'},
+
+  init: function () {
+    var soundEl = this.el;
+
+    soundEl.addEventListener('sound-ended', function () {
+      restart();
+    });
+  }
+});
+
+function restart() {
+  document.getElementById('scene1').setAttribute('visible', 'true');
+  document.getElementById('scene2').setAttribute('visible', 'false');
+  countdown.setAttribute('visible', 'false');
+  resetCamera();
+  enableDot();
+}
+
+function startScene2() {
+  setScene2Camera();
+  document.getElementById('scene1').setAttribute('visible', 'false');
+  document.getElementById('scene2').setAttribute('visible', 'true');
+  var entity = document.querySelector('[sound]');
+  entity.components.sound.playSound();
+}
+
+function disableDot() {
+  dot.setAttribute('visible', 'false');
+  dot.setAttribute('position', '0 100 10');
+}
+
+function enableDot() {
+  dot.setAttribute('visible', 'true');
+  dot.setAttribute('position', '0 5 -10');
+}
+
+function resetCamera() {
+  let controls = camera.components['look-controls'];
+  controls.pitchObject.rotation.x = 0;
+  controls.yawObject.rotation.y = 0;
+
+  cameraRig.setAttribute('position', '0 1.6 0');
+  camera.setAttribute('position', '0 0 0');
+  camera.removeAttribute("alongpath");
+}
+
+function setScene2Camera() {
+  resetCamera();
+  cameraRig.setAttribute('position', '0 1.6 20');
+  camera.setAttribute("alongpath", "curve", "#track1");
+}
+
