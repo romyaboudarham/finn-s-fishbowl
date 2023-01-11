48d45029a183cd8468a12dfadafe61ba266600de
Romy Aboudarham <romy@Romy-Aboudarhams-MacBook-Pro.local>
2023-01-11 15:45:37 -0800

    cleaned up story pages and models

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function toggle_visibility(btnId, displayId) {
     var btn = document.getElementById(btnId);
     var display = document.getElementById(displayId);
-    if(display.style.display == 'block') {
-        btn.value = "Show Audio Transcription";
-        display.style.display = 'none';
-    } else {
-        btn.value = "Hide Audio Transcription";
-        display.style.display = 'block';
+    if (btn.id == "transcribe-btn") {
+        if(display.style.display == 'block') {
+            btn.value = "Show Audio Transcription";
+            display.style.display = 'none';
+        } else {
+            btn.value = "Hide Audio Transcription";
+            display.style.display = 'block';
+        }
+    } else if (btn.id == "access-btn") {
+        if(display.style.display == 'block') {
+            btn.value = "Show \u267F Instructions";
+            display.style.display = 'none';
+        } else {
+            btn.value = "Hide \u267F Instructions";
+            display.style.display = 'block';
+        }
     }
 }
