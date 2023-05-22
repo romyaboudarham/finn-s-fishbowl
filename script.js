1d66e1f39fdf7b67be76199270d3cffff4d53790
Romy Aboudarham <romy@Romy-Aboudarhams-MacBook-Pro.local>
2023-05-22 11:02:46 -0700

    made adjustments for the transitions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let aud = document.getElementById('audio');
+var arrow = document.getElementById("follow-arrow");
+var ch = document.getElementById("chapter-div");
+aud.onended = function() {
+    ch.style.display = 'none';
+    arrow.style.display = 'block';
+};
+
 function toggle_visibility(btnId, displayId) {
     var btn = document.getElementById(btnId);
     var display = document.getElementById(displayId);
